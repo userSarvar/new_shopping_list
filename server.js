ffcbdd85e84ab45384ec0c73a1576a360910e53e
userSarvar <itspecialist2200@gmail.com>
2024-08-12 13:01:35 +0500

    new srver.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-import app from './app';
-import config from './config';
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const { PORT } = config;
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+// The "catchall" handler: for any request that doesn't match a route, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
